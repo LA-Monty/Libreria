2876eabd10be8344f17199cc38dfbb55dcf4db00
LA-Monty <20230919@itla.edu.do>
2025-11-28 20:30:25 -0400

    Practica finalizada

---
+++ modelo/conexion.php
@@ -0,0 +1,8 @@
+<?php
+
+//Conexion a la base de datos 
+$conexion = new mysqli("localhost", "root", "", "libreria","3306");
+$conexion->set_charset("utf8")
+?>
+
+<!--Cambios necesarios para no joder el repositorio-->
